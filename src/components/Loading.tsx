c173764359e4706c20af3408c6c7f8f33437c85b
DolphinLin <hisb50918@gmail.com>
2021-04-20 10:18:45 +0800

    add loading spinner

---
+++ src/components/Loading.tsx
@@ -0,0 +1,21 @@
+import styled, { keyframes } from 'styled-components';
+
+const rotate = keyframes`
+    from {
+        transform: rotate(0deg);
+    }
+    to {
+        transform: rotate(360deg);
+    }
+`;
+
+const Loading = styled.div`
+  border: 4px solid #f3f3f3; /* Light grey */
+  border-top: 4px solid #3498db; /* Blue */
+  border-radius: 50%;
+  width: 20px;
+  height: 20px;
+  animation: ${rotate} 2s linear infinite;
+`;
+
+export default Loading;
