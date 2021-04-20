29b1d5c4bebf187660ed015cf5fd7cb170185d46
DolphinLin <hisb50918@gmail.com>
2021-04-20 10:17:10 +0800

    add type

---
+++ src/services/types.ts
@@ -0,0 +1,26 @@
+export interface RepoItem {
+  id: number;
+  node_id: string;
+  name: string;
+  full_name: string;
+  private: boolean;
+  owner: {
+    login: string;
+    id: number;
+    node_id: string;
+    avatar_url: string;
+  };
+  description: string;
+  created_at: string;
+  updated_at: string;
+  pushed_at: string;
+  size: number;
+  stargazers_count: number;
+  watchers_count: number;
+  language: string;
+  forks_count: number;
+  html_url: string;
+  archived: false;
+  disabled: false;
+  open_issues_count: number;
+}
