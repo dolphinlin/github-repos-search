a9f84f41a8e7554d18320503a083b7f05869fe0b
DolphinLin <hisb50918@gmail.com>
2021-04-20 10:17:45 +0800

    add common config

---
+++ src/services/config.ts
@@ -0,0 +1,6 @@
+export const REPO_SIZE = 10;
+export const MAX_REPO_SIZE = 1000; // github API limiation
+export const KEYWORD_DEBOUNCED_TIME = 500;
+
+// For unauthenticated requests, the rate limit allows you to make up to 10 requests per minute.
+export const TASK_SLEEP_TIME = (60 * 1000) / 7;
